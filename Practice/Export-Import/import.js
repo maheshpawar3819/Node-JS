aed322020d45cdb4282e36d9f3bce03b3319a215
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-13 00:01:35 +0530

    add npm package figlet

--- Practice/Export-Import/import.js
+++ Practice/Export-Import/import.js
@@ -2,11 +2,11 @@
 
 // require() : a bulid in function to include external modules that exist in saperate files.
 
-let get=require("./Fruits");
+let get = require("./Fruits");
 const { color } = require("./Fruits/apple");
 
-console.log(get)
-console.log(get[0].fruit,color)
+console.log(get);
+console.log(get[0].fruit, color);
 // console.log(get.Pi)
 // console.log(get.sum(5,5))
 
