ab72dd31dcbb03edd5d74263b9eb8604dbedf756
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-16 23:54:14 +0530

    add path and learn about sending response

--- Express_js/index.js
+++ Express_js/index.js
@@ -7,6 +7,24 @@ app.listen(port, () => {
   console.log("Server Started On", port);
 });
 
-app.use((req, res) => {
-  console.log("new request received");
+app.get("/", (req, res) => {
+  res.send("Hello this is the  root path");
 });
+
+app.get("/apple", (req, res) => {
+  res.send("this is the apple path");
+});
+
+app.get("/banana", (req, res) => {
+  res.send("this is the banana path");
+});
+
+//when the rout can not match then show this default response
+app.get("*", (req, res) => {
+  res.send("this is unknown path");
+});
+
+// app.use((req, res) => {
+//   console.log("new request received");
+//   res.send("<h1>hii mahesh pawar how are you</h1>");
+// });
