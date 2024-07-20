a2e8553447a695bc0aa506c986555d58b465fe1f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-20 19:56:42 +0530

    add file wirte file and append operation perform on file

--- fileoperations(thapa tech)/index.js
+++ fileoperations(thapa tech)/index.js
@@ -1,5 +1,9 @@
 let fs=require("fs");
 
-fs.writeFileSync("note.txt","this is my first file creation operation");
+// fs.writeFileSync("note.txt","this is my first file creation operation");
 
-fs.writeFileSync("note.txt","try to try to modify text");
+// fs.writeFileSync("note.txt","try to try to modify text");
+
+// fs.writeFileSync("note.txt","how are you mahesh");
+
+fs.appendFileSync("note.txt" , "\n I am fine :)")
