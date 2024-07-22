06891085653325b9692cd4aa9d06b200fead0864
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-22 11:53:53 +0530

    Create Crud Operations

---
+++ fileoperations(thapa tech)/Crudfileop/index.js
@@ -0,0 +1,16 @@
+const fs=require("fs");
+
+// fs.mkdirSync("Crud");
+
+// fs.writeFileSync("./Crud/bio.txt","Data");
+
+// fs.appendFileSync("./Crud/bio.txt","\n Mahesh");
+
+// const data=fs.readFileSync("./Crud/bio.txt","utf-8")
+// console.log(data);
+
+// fs.renameSync("./Crud/bio.txt", "./Crud/mybio.txt");
+
+// fs.unlinkSync("./Crud/mybio.txt");
+
+fs.rmdirSync("Crud");
