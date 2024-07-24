e5c957c2bee53b9e0ff75d9aa68450db533167c8
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-24 11:53:35 +0530

    add password validetor

--- fileoperations(thapa tech)/Moduls/Npm/index.js
+++ fileoperations(thapa tech)/Moduls/Npm/index.js
@@ -1,4 +1,26 @@
-const validator=require("validator");
-
-const res=validator.isEmail("mahipawar@gmail.com");
+const validator = require("validator");
+const passvalidator = require("password-validator");
+const PasswordValidator = require("password-validator");
+const res = validator.isEmail("mahipawar@gmail.com");
 console.log(res);
+
+let schema = new PasswordValidator();
+
+schema;
+schema
+  .is()
+  .min(8) // Minimum length 8
+  .is()
+  .max(16) // Maximum length 16
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(1) // Must have at least 1 digit
+  .has()
+  .not()
+  .spaces(); // Should not have spaces
+
+  const checkvalidp=schema.validate("Mahi8888");
+  console.log(checkvalidp);
