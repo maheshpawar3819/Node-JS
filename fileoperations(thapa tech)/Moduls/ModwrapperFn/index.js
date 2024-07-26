706d5c2a4be7388db05506bd4ab506dc64c70b9e
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-26 10:48:45 +0530

    add module wrapper function

---
+++ fileoperations(thapa tech)/Moduls/ModwrapperFn/index.js
@@ -0,0 +1,12 @@
+//Module Wrapper Function is the function just like IEFE - Immediatly Envoked Function Expression the scope is private scope behind seends node will be wrapup all code in the Module Wrapper Fuction.
+
+/*
+ This paramenters are found in module wrapper function 
+    1.require
+    2.export
+    4.filename
+    5.dirname
+*/
+
+console.log(__dirname);
+console.log(__filename)
