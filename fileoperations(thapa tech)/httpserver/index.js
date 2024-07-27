75b9ab981f2686ee952889c349cff721fe031dd6
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-27 12:07:38 +0530

    add json deta

--- fileoperations(thapa tech)/httpserver/index.js
+++ fileoperations(thapa tech)/httpserver/index.js
@@ -1,8 +1,7 @@
 //Creating the http server without using express
-
 const http = require("http");
 const { escape } = require("querystring");
-
+const fs = require("fs");
 const port = 8080;
 
 const server = http.createServer((req, res) => {
@@ -10,13 +9,18 @@ const server = http.createServer((req, res) => {
   // console.log(req.url)
   if (req.url == "/") {
     res.end("Hello from the server side :)");
-  }else if(req.url =="/aboutus"){
+  } else if (req.url == "/aboutus") {
     res.end("hello from about us page :)");
-  }else if(req.url == "/contact"){
+  } else if (req.url == "/contact") {
     res.end("hello from contact page :)");
-  }else{
-    res.writeHead(404, {"content-type" : "text/html"})
-    res.end("<h1>404 Error Page does not exist</h1>"); 
+  } else if (req.url == "/userapi") {
+    fs.readFile(`${__dirname}/Userapi/userapi.json`, "utf-8", (error, data) => {
+      console.log(data);
+    });
+    res.end("hello from user api side");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>404 Error Page does not exist</h1>");
   }
 });
 
