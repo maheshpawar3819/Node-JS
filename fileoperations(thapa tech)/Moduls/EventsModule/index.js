a3a4c7b7c8eadeeab731eb68ccb126e8e67c8d5d
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-29 11:48:18 +0530

    add events module

--- fileoperations(thapa tech)/Moduls/EventsModule/index.js
+++ fileoperations(thapa tech)/Moduls/EventsModule/index.js
@@ -3,6 +3,8 @@
 //where you can create- , fire -, and listen for - your own events 
 
 //Ex : Registrating for the event to be fired only one time using once
+//Ex 2 : Create an event emmiter instance and register a couple of callbacks 
+//Ex 3 : Registaring for a event with callback parameters
 
 const EventEmitter=require("events");
 
@@ -10,8 +12,25 @@ const event=new EventEmitter();
 
 // Createating my own event 
 
-event.on("sayMyName",() => {
-    console.log("My name is Mahesh");
+// event.on("sayMyName",() => {
+//     console.log("My name is Mahesh");
+// })
+
+// event.on("sayMyName",() => {
+//     console.log("My name is Sanket");
+// })
+
+// event.on("sayMyName",() => {
+//     console.log("My name is suraj")
+// })
+
+// Ex 3 : 
+
+event.on("checkPage",(status,msg) => {
+    console.log(`Status code is ${status} and page is ${msg}`);
 })
 
-event.emit("sayMyName");
+// event.emit("sayMyName");
+event.emit("checkPage",200,"ok");
+
+
