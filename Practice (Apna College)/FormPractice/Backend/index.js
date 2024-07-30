9078bbd122e084db5d951f6199ad9aa1352c3dba
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-30 12:05:01 +0530

    create simple html form and handle requests using backend express server

--- Practice (Apna College)/FormPractice/Backend/index.js
+++ Practice (Apna College)/FormPractice/Backend/index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/register",(req,res) => {
-    res.send("standard get response");
-  
-})
+//for post request to read url data(middleware)
+app.use(express.urlencoded({ extended: true }));
+//if is in json format(middleware)
+app.use(express.json());
 
-app.post("/register",(req,res) => {
-    res.send("standard post response");
-})
+app.get("/register", (req, res) => {
+  let { user, password } = req.query;
+  //   console.log(req.query)
+  res.send(`Standart get request Welcome ${user}!`);
+});
 
-app.listen(port,() => {
-    console.log("Listning on port :",port);
-})
+app.post("/register", (req, res) => {
+  //   console.log(req.body)
+  let { user, password } = req.body;
+  res.send(`Standart post request Welcome ${user}` );
+});
+
+app.listen(port, () => {
+  console.log("listning on port : ", port);
+});
