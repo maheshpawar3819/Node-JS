a769e6f6221fce7ecc399e7d4ed9ac481652373f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-07 11:59:37 +0530

    add 2nd api create a form and add successfuly data in the array data

--- Practice (Apna College)/Rest_Class/index.js
+++ Practice (Apna College)/Rest_Class/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use (express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
@@ -28,6 +28,17 @@ let posts = [
 
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  // console.log(req.body);
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.send("post request working fine");
 });
 
 app.listen(port, () => {
