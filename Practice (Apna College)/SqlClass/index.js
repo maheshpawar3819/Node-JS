a4b54691cb3fe965db47c2da27e330b2f8b73a95
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-15 11:00:41 +0530

    add package mysql2 for connection of database and protext password

--- Practice (Apna College)/SqlClass/index.js
+++ Practice (Apna College)/SqlClass/index.js
@@ -1,4 +1,24 @@
 const { faker } = require("@faker-js/faker");
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "demo",
+  password: process.env.DB_PASSWORD,
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
 
 const getRandomUser = () => {
   return {
@@ -9,4 +29,4 @@ const getRandomUser = () => {
   };
 };
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
