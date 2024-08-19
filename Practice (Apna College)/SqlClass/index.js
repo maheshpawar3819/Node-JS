c7a8ad7316729c99afac61f2f637c865ae0cf614
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-19 11:50:17 +0530

    setup express

--- Practice (Apna College)/SqlClass/index.js
+++ Practice (Apna College)/SqlClass/index.js
@@ -1,13 +1,24 @@
 const { faker } = require("@faker-js/faker");
 require("dotenv").config();
 const mysql = require("mysql2");
+const express=require("express");
+const app=express();
+const port=8080;
+
+app.listen(port,()=> {
+  console.log("Server is listning",port);
+})
+
+app.get("/",(req,res) => {
+  res.send("welcome to home page");
+})
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "demo",
   password: process.env.DB_PASSWORD,
-});
+}); 
 
 const getRandomUser = () => {
   return [
@@ -18,24 +29,24 @@ const getRandomUser = () => {
   ];
 };
 
-//ADD DETA INTO THE TABLE
-let qu = "INSERT INTO user (id,username,email,password) VALUES ?";
-let data = [];
+// //ADD DETA INTO THE TABLE
+// let qu = "INSERT INTO user (id,username,email,password) VALUES ?";
+// let data = [];
 
-for (let i = 1; i <= 100; i++) {
-  data.push(getRandomUser()); //give 100 random fake users;
-}
+// for (let i = 1; i <= 100; i++) {
+//   data.push(getRandomUser()); //give 100 random fake users;
+// }
 
-try {
-  connection.query(qu, [data], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    // console.log(result.length);
-  });
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   connection.query(qu, [data], (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     // console.log(result.length);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
 
-connection.end();
+// connection.end();
 
 // console.log(getRandomUser());
