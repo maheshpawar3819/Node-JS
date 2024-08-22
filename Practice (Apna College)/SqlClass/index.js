3387b1fc097210c96b8c91baf2e4cf97c26eb785
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-22 11:25:28 +0530

    add patch requrest to update the username pending some work

--- Practice (Apna College)/SqlClass/index.js
+++ Practice (Apna College)/SqlClass/index.js
@@ -4,9 +4,11 @@ const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const path = require("path");
-const { chown } = require("fs");
+const methodOverride = require("method-override");
 const port = 8080;
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -29,20 +31,42 @@ app.get("/", (req, res) => {
   }
 });
 
-//rout to show data of users
+//Route to show data of users
 
-app.get("/user",(req,res) => {
-  let q=`SELECT * FROM user`
-  try{
-    connection.query(q,(err,users) => {
-      if(err) throw err;
-      res.render("showusers.ejs",{users});
-    })
-  }catch (err){
+app.get("/user", (req, res) => {
+  let q = `SELECT * FROM user`;
+  try {
+    connection.query(q, (err, users) => {
+      if (err) throw err;
+      res.render("showusers.ejs", { users });
+    });
+  } catch (err) {
     console.log(err);
     res.send(`Someting wrong in detabase`);
   }
-})
+});
+
+//edit Route
+
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("edit.ejs", { user });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Update Route
+
+app.patch("/user/:id", (req, res) => {
+  res.send("updated");
+});
 
 const connection = mysql.createConnection({
   host: "localhost",
