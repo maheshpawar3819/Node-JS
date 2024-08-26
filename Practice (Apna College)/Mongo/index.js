3048fb4844ad73ef94fba24ed05495e50afdd68b
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-26 12:18:52 +0530

    find in mongoose add 3 methods of find

--- Practice (Apna College)/Mongo/index.js
+++ Practice (Apna College)/Mongo/index.js
@@ -23,17 +23,42 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 // const Employee=mongoose.model("Employee",userSchema);
 
+//find by id 
+
+// User.findById("66cc1ed14365d4588140f97d").then((res) => {
+//     console.log(res);
+// }).catch(() => {
+//     console.log("something wrong in db");
+// })
+
+// find by queery
+// User.findOne({age:{$gt:21}}).then((res) => {
+//     console.log(res);
+// }).catch(() => {
+//     console.log("Something wrong in db");
+// })
+
+
+//find all documents
+// User.find({}).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err,"Something wrong");
+// })
+
 //Insert Multiple Data
 
-User.insertMany([
-    {name:"Sairaj",email:"sairaj48@gmail.com",age:22,city:"Pune"},
-    {name:"Shrinath",email:"Shris@gmail.com",age:21,city:"Baramati"},
-    {name:"Rohan",email:"rohansg@outlook.com",age:21,city:"Phaltan"}
-]).then((data) => {
-    console.log(data)
-}).catch((err) => {
-    console.log(err,"Something wrong");
-})
+// User.insertMany([
+//   { name: "Sairaj", email: "sairaj48@gmail.com", age: 22, city: "Pune" },
+//   { name: "Shrinath", email: "Shris@gmail.com", age: 21, city: "Baramati" },
+//   { name: "Rohan", email: "rohansg@outlook.com", age: 21, city: "Phaltan" },
+// ])
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err, "Something wrong");
+//   });
 
 // const user1 = new User({
 //   name: "Mahesh",
