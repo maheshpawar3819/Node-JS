fde4bdade50518349d32e12958d3ff62c43affdc
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-26 10:48:18 +0530

    get mongoose package and connet to mongoDB Server

---
+++ Practice (Apna College)/Mongo/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+main()
+  .then((result) => {
+    // console.log(result)
+    console.log("connect to mongodb Success..");
+  })
+  .catch((err) => {
+    console.log(err, "someting wrong");
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
