f7c2d34871f886546806f5c95bc5cb5b85d612d8
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-26 11:56:25 +0530

    insert multiple deta use insertMany method to insert data

--- Practice (Apna College)/Mongo/index.js
+++ Practice (Apna College)/Mongo/index.js
@@ -13,42 +13,57 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
-const userSchema=mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number,
-    city:String
-})
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  city: String,
+});
 
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 // const Employee=mongoose.model("Employee",userSchema);
 
-const user1=new User({
-    name:"Mahesh",
-    email:"mahesh56@gmail.com",
-    age:23,
-    city:"Pune"
+//Insert Multiple Data
+
+User.insertMany([
+    {name:"Sairaj",email:"sairaj48@gmail.com",age:22,city:"Pune"},
+    {name:"Shrinath",email:"Shris@gmail.com",age:21,city:"Baramati"},
+    {name:"Rohan",email:"rohansg@outlook.com",age:21,city:"Phaltan"}
+]).then((data) => {
+    console.log(data)
+}).catch((err) => {
+    console.log(err,"Something wrong");
 })
 
-const user2=new User({
-    name:"Samir",
-    email:"samirb54@gmail.com",
-    age:22,
-    city:"Kolhapur"
-})
+// const user1 = new User({
+//   name: "Mahesh",
+//   email: "mahesh56@gmail.com",
+//   age: 23,
+//   city: "Pune",
+// });
 
-const user3=new User({
-    name:"Shubham",
-    email:"Shubhama3@gmail.com",
-    age:20,
-    city:"Phaltan"
-})
+// const user2 = new User({
+//   name: "Samir",
+//   email: "samirb54@gmail.com",
+//   age: 22,
+//   city: "Kolhapur",
+// });
+
+// const user3 = new User({
+//   name: "Shubham",
+//   email: "Shubhama3@gmail.com",
+//   age: 20,
+//   city: "Phaltan",
+// });
 
 //save method returns a promice
-user1.save();
-// user2.save();
-user3.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err,"Someting Wrong");
-})
+// user1.save();
+// // user2.save();
+// user3
+//   .save()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err, "Someting Wrong");
+//   });
