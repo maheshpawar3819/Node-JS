230807b2611ae1a7a2b5b6a34c6f5cfb6b9ef5c3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-28 19:33:06 +0530

    create middleware to log information

--- Practice (Apna College)/Middlewares/index.js
+++ Practice (Apna College)/Middlewares/index.js
@@ -2,19 +2,26 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use((req,res,next) =>{
-    console.log("hii i am middleware");
-    next();
-})
+// app.use((req,res,next) =>{
+//     console.log("hii i am middleware");
+//     next();
+// })
 
+// creating utility middleware(usefull middleware);
+//middleware to log information (requrest hostname time and date);
+app.use((req, res, next) => {
+  req.time = new Date(Date.now()).toString();
+  console.log(req.method, req.hostname, req.time);
+  next();
+});
 
-app.get("/",(req,res) => {
-    res.send("welcome to home page:)");
-})
+app.get("/", (req, res) => {
+  res.send("welcome to home page:)");
+});
 
-app.get("/random",(req,res) => {
-    res.send("i am random page");
-})
+app.get("/random", (req, res) => {
+  res.send("i am random page");
+});
 
 app.listen(port, () => {
   console.log("Listning on Port :", port);
